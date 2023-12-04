859c51fb2271c3ce198479a57dacc5d11f27384f
matthew-yen <83885235+matthew-yen@users.noreply.github.com>
2023-12-03 19:41:11 -0800

    whiptail interface

--- main.sh
+++ main.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 #MIT Licence
 #Copyright (c) Matthew Yen
+CHOICE=$(whiptail --title "System Configurations" --menu "Choose an option" 25 78 16 \
+"<-- Back" "Return to the main menu." \
+"Add users" "Add users to the system." \
+"Modify User" "Modify an existing user." \
+"List Users" "List all users on the system." \
+"Add Group" "Add a user group to the system." \
+"Modify Group" "Modify a group and its list of members." \
+"List Groups" "List all groups on the system." 3>&1 1>&2 2>&3)
